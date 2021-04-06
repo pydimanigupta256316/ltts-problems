a130595747d4b03c2def29de9193da1c90734b78
pydimanigupta256316 <80501821+pydimanigupta256316@users.noreply.github.com>
2021-04-06 18:34:23 +0530

    Create main.c

---
+++ question 2a/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ * @author manigupta
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    read("data.csv");
+    
+    disp(arr);
+    sort(arr);
+}
